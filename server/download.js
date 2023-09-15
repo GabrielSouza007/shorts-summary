c1851d19d35b5a6b785860080033a4169ab4f80d
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2023-09-14 21:25:28 -0300

    update project :tada:

--- server/download.js
+++ server/download.js
@@ -1,7 +1,8 @@
 import ytdl from 'ytdl-core'
 import fs from 'fs'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise((resolve, reject) => {
+
     const videoURL = "https://www.youtube.com/shorts/" + videoId
     console.log("Realizando o download do vídeo:", videoId)
 
@@ -15,8 +16,13 @@ export const download = (videoId) => {
         }
     ).on("end", () => {
         console.log("Download do vídeo finalizado.")
+        resolve()
     })
-    .on("error", () => {
-        console.log("Não foi possível realizar o download do vídeo. Detalhes do erro:", error)
-    }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+        .on("error", () => {
+            console.log("Não foi possível realizar o download do vídeo. Detalhes do erro:", error)
+            reject(error)
+        })
+        .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+})
+
+
